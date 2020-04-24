1bd99d17feb3a9445abf618bf617125166403355
AminHoss <47458405+AminHoss@users.noreply.github.com>
2020-04-24 13:49:26 -0400

    Update Square_Color.py

--- Square_Color.py
+++ Square_Color.py
@@ -1,7 +1,8 @@
 column, row = input("Please enter the coordinates of your square: ")
 letter_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
-#This might need some explanation, 
 column_parity = (letter_list.index(column)+ 1) % 2
+#The reason there is a +1 for the index function is because the list starts at 0, so the 1st row would be counted as an even row, 
+#while it is an odd row, so the +1 fixes this problem.
 row_parity = int(row) % 2
 if column_parity == row_parity:
     print("The square is black")
